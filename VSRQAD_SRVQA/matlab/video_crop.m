ba924a47cb8cd63458b4ccd0b95550d5fa5466c9
Sheng Wei <2200432029@email.szu.edu.cn>
2024-05-07 12:31:17 +0800

    Delete VSRQAD_SRVQA directory

--- VSRQAD_SRVQA/matlab/video_crop.m
+++
@@ -1,7 +0,0 @@
-% 使用FFmpeg裁剪视频
-system('ffmpeg -i K:\video_jitter\video007\x8\video007_29fps_04_x8_01.mp4 -filter:v "crop=300:200:400:300" K:\video_jitter\video007\x8\video007_x8_output.mp4')
-%% 宽 高 左上顶点
-
-% system('ffmpeg -i K:\video_jitter\video008\x2\video008_29fps_09_x2_03.mp4 -filter:v "crop=800:500:1200:5" K:\video_jitter\video008\x2\video008_x2_output.mp4')
-
-% system('ffmpeg -i K:\video_jitter\video222\x2\video222_29fps_05_x2_03.mp4 -filter:v "crop=200:200:500:600" K:\video_jitter\video222\x2\video222_x2_output.mp4')
